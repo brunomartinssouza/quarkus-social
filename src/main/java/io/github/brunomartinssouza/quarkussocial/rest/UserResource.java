978dfb5a2a59ecc98f51c3595bff174294f09164
Bruno Martins <martins_bruno01@hotmail.com>
2023-12-02 22:41:31 -0300

    criação do projeto

---
+++ src/main/java/io/github/brunomartinssouza/quarkussocial/rest/UserResource.java
@@ -0,0 +1,24 @@
+package io.github.brunomartinssouza.quarkussocial.rest;
+
+import io.github.brunomartinssouza.quarkussocial.rest.dto.CreateUserRequest;
+import jakarta.ws.rs.*;
+
+
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class UserResource {
+
+    @POST
+    public Response createUser(CreateUserRequest userRequest){
+        return Response.ok(userRequest).build();
+    }
+
+    @GET
+    public Response listAllUsers(){
+        return Response.ok().build();
+    }
+}
