10f339b8144d04107fd203784a188bd86736b44d
Bruno Martins <martins_bruno01@hotmail.com>
2023-12-02 23:36:20 -0300

    Create e list do User

--- src/main/java/io/github/brunomartinssouza/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/brunomartinssouza/quarkussocial/rest/UserResource.java
@@ -1,6 +1,10 @@
 package io.github.brunomartinssouza.quarkussocial.rest;
 
+import io.github.brunomartinssouza.quarkussocial.domain.model.User;
 import io.github.brunomartinssouza.quarkussocial.rest.dto.CreateUserRequest;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 
 
@@ -13,12 +17,21 @@ import jakarta.ws.rs.core.Response;
 public class UserResource {
 
     @POST
+    @Transactional
     public Response createUser(CreateUserRequest userRequest){
-        return Response.ok(userRequest).build();
+
+        User user = new User();
+        user.setAge(userRequest.getAge());
+        user.setName(userRequest.getName());
+
+        user.persist();
+
+        return Response.ok(user).build();
     }
 
     @GET
     public Response listAllUsers(){
-        return Response.ok().build();
+        PanacheQuery<PanacheEntityBase> query = User.findAll();
+        return Response.ok(query.list()).build();
     }
 }
