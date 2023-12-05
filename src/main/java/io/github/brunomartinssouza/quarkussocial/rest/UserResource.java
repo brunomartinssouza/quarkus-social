9783a2b4539fac65d27a9bc0143b45252a852ec7
Bruno Martins <martins_bruno01@hotmail.com>
2023-12-05 00:17:16 -0300

    Criação das validações e tratamento de erros

--- src/main/java/io/github/brunomartinssouza/quarkussocial/rest/UserResource.java
+++ src/main/java/io/github/brunomartinssouza/quarkussocial/rest/UserResource.java
@@ -3,15 +3,20 @@ package io.github.brunomartinssouza.quarkussocial.rest;
 import io.github.brunomartinssouza.quarkussocial.domain.model.User;
 import io.github.brunomartinssouza.quarkussocial.domain.repository.UserRepository;
 import io.github.brunomartinssouza.quarkussocial.rest.dto.CreateUserRequest;
+import io.github.brunomartinssouza.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
 import jakarta.ws.rs.*;
 
 
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.Set;
 
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -21,9 +26,18 @@ public class UserResource {
     @Inject
     UserRepository userRepository;
 
+    @Inject
+    Validator validate;
+
     @POST
     @Transactional
     public Response createUser(CreateUserRequest userRequest){
+
+        Set<ConstraintViolation<CreateUserRequest>> violations = this.validate.validate(userRequest);
+        if(!violations.isEmpty()){
+            ResponseError responseError = ResponseError.createFromValidation(violations);
+            return Response.status(400).entity(responseError).build();
+        }
 
         User user = new User();
         user.setAge(userRequest.getAge());
