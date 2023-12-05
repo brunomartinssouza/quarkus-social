98b3d83900d612a6637d37cf020b3f6e362945f6
Bruno Martins <martins_bruno01@hotmail.com>
2023-12-05 00:55:13 -0300

    Inclusão da biblioteca Project Lombok

--- src/main/java/io/github/brunomartinssouza/quarkussocial/rest/dto/CreateUserRequest.java
+++ src/main/java/io/github/brunomartinssouza/quarkussocial/rest/dto/CreateUserRequest.java
@@ -2,7 +2,9 @@ package io.github.brunomartinssouza.quarkussocial.rest.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+@Data
 public class CreateUserRequest {
 
     @NotBlank(message = "Name is required")
@@ -10,19 +12,4 @@ public class CreateUserRequest {
     @NotNull(message = "Age is required")
     private Integer age;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
