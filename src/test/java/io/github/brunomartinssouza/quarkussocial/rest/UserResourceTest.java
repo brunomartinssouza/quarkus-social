3986dfd56e6bd6a0849314f2f858871d9c908231
Bruno Martins <martins_bruno01@hotmail.com>
2024-01-04 18:40:48 -0300

    Ordenação dos testes de Users

--- src/test/java/io/github/brunomartinssouza/quarkussocial/rest/UserResourceTest.java
+++ src/test/java/io/github/brunomartinssouza/quarkussocial/rest/UserResourceTest.java
@@ -2,12 +2,14 @@ package io.github.brunomartinssouza.quarkussocial.rest;
 
 import io.github.brunomartinssouza.quarkussocial.rest.dto.CreateUserRequest;
 import io.github.brunomartinssouza.quarkussocial.rest.dto.ResponseError;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -15,10 +17,15 @@ import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserResourceTest {
+
+    @TestHTTPResource("/users")
+    URL apiURL;
 
     @Test
     @DisplayName("Should create an user successfully")
+    @Order(1)
     public void createUserTest(){
         var user = new CreateUserRequest();
         user.setName("Fulano");
@@ -26,12 +33,12 @@ class UserResourceTest {
 
         var response =
                 given()
-                    .contentType(ContentType.JSON)
-                    .body(user)
-                .when()
-                    .post("/users")
-                .then()
-                    .extract().response();
+                        .contentType(ContentType.JSON)
+                        .body(user)
+                        .when()
+                        .post(apiURL)
+                        .then()
+                        .extract().response();
 
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
@@ -39,6 +46,7 @@ class UserResourceTest {
 
     @Test
     @DisplayName("Should return error when json is not valid")
+    @Order(2)
     public void createUserValidationErrorTest(){
         var user = new CreateUserRequest();
         user.setName(null);
@@ -49,7 +57,7 @@ class UserResourceTest {
                         .contentType(ContentType.JSON)
                         .body(user)
                         .when()
-                        .post("/users")
+                        .post(apiURL)
                         .then()
                         .extract().response();
 
@@ -64,4 +72,17 @@ class UserResourceTest {
 
     }
 
+    @Test
+    @DisplayName("Should list all users")
+    @Order(3)
+    public void listAllUserTest() {
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(apiURL)
+                .then()
+                .statusCode(200)
+                .body("size()", Matchers.is(1));
+    }
 }
