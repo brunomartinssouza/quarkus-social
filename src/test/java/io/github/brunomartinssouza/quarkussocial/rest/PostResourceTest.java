a712cb726a639554e2becde3529d82d8327846e1
Bruno Martins <martins_bruno01@hotmail.com>
2024-02-02 21:46:05 -0300

    Finalização dos testes de Posts

--- src/test/java/io/github/brunomartinssouza/quarkussocial/rest/PostResourceTest.java
+++ src/test/java/io/github/brunomartinssouza/quarkussocial/rest/PostResourceTest.java
@@ -1,6 +1,10 @@
 package io.github.brunomartinssouza.quarkussocial.rest;
 
+import io.github.brunomartinssouza.quarkussocial.domain.model.Follower;
+import io.github.brunomartinssouza.quarkussocial.domain.model.Post;
 import io.github.brunomartinssouza.quarkussocial.domain.model.User;
+import io.github.brunomartinssouza.quarkussocial.domain.repository.FollowerRepository;
+import io.github.brunomartinssouza.quarkussocial.domain.repository.PostRepository;
 import io.github.brunomartinssouza.quarkussocial.domain.repository.UserRepository;
 import io.github.brunomartinssouza.quarkussocial.rest.dto.CreatePostRequest;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
@@ -22,18 +26,52 @@ class PostResourceTest {
 
     @Inject
     UserRepository userRepository;
+    @Inject
+    FollowerRepository followerRepository;
+    @Inject
+    PostRepository postRepository;
+
+
     Long userId;
+    Long userNotFollowerId;
+    Long userFollowerId;
     Long inexistentUserId = 999l;
 
     @BeforeEach
     @Transactional
     public void steUP(){
+        //usuário padrão dos testes
         var user = new User();
         user.setAge(30);
         user.setName("Fulano");
-
         userRepository.persist(user);
         userId = user.getId();
+
+        //usuário que não segue
+        var userNotFollower = new User();
+        userNotFollower.setAge(33);
+        userNotFollower.setName("Cicrano");
+        userRepository.persist(userNotFollower);
+        userNotFollowerId = userNotFollower.getId();
+
+        //usuário que segue
+        var userFollower = new User();
+        userFollower.setAge(31);
+        userFollower.setName("Terceiro");
+        userRepository.persist(userFollower);
+        userFollowerId = userFollower.getId();
+
+        Follower follower = new Follower();
+        follower.setUser(user);
+        follower.setFollower(userFollower);
+        followerRepository.persist(follower);
+
+        //cirada a postagem para o usuário
+        Post post = new Post();
+        post.setText("Hello");
+        post.setUser(user);
+        postRepository.persist(post);
+
     }
 
 
@@ -99,7 +137,7 @@ class PostResourceTest {
     }
 
     @Test
-    @DisplayName("should return 400 when follower doesn't exist")
+    @DisplayName("should return 404 when follower doesn't exist")
     public void listPostFollowerNotFoundTest(){
 
         var inexistentFollowerId = 999;
@@ -116,5 +154,35 @@ class PostResourceTest {
 
     }
 
+    @Test
+    @DisplayName("should return 403 when follower isn't a follower")
+    public void listPostNotAFollowerTest(){
+
+        given()
+            .contentType(ContentType.JSON)
+            .pathParam("userId", userId)
+            .header("followerId",userNotFollowerId)
+        .when()
+            .get()
+        .then()
+            .statusCode(403)
+            .body(Matchers.is("You can't see these posts"));
+
+    }
+
+    @Test
+    @DisplayName("should return posts")
+    public void listPostTest(){
+
+        given()
+            .contentType(ContentType.JSON)
+            .pathParam("userId", userId)
+            .header("followerId",userFollowerId)
+        .when()
+            .get()
+        .then()
+            .statusCode(200)
+            .body("size()", Matchers.is(1));
+    }
 
 }
